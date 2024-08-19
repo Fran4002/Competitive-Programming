3b267ca98e0e02bcfc9fcfc09f6f526c8bd049f6
Francisco Jesus Hernandez Hernandez <a353691@alumnos.uaslp.mx>
2024-08-18 21:17:08 -0600

    New template

---
+++ CSES/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(),x.end()
+#define readv(x) for (auto &v: x) cin >> x;
+#define printv(x) for (auto &v: x) cout << x;
+
+using ll = long long int;
+using ull = unsigned long long int;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+using vc = vector<char>;
+
+void solve () {
+  
+}
+
+int main () {
+  cin.tie(0) -> ios_base::sync_with_stdio(0);
+
+  int t = 1;
+
+  cin >> t;
+
+  for (int i = 1; i <= t; i++) {
+    //cout << "Test #" << i << ":\n";
+    solve();
+  }
+}
